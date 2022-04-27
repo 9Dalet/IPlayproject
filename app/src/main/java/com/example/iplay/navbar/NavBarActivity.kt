e347bf838d26e9874f33cfccd4eb66a3e9214c4a
9Dalet <daniele.giarratano04@gmail.com>
2022-04-27 16:43:04 +0200

    Aggiunta S in Sport

--- app/src/main/java/com/example/iplay/navbar/NavBarActivity.kt
+++ app/src/main/java/com/example/iplay/navbar/NavBarActivity.kt
@@ -2,6 +2,7 @@ package com.example.iplay.navbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.iplay.R
 
 class NavBarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
