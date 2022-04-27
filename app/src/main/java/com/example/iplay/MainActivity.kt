95a7289854ab318f10cd61e863e4215d7f1777ee
SimoPed <simone.pedrini12@gmail.com>
2022-04-27 17:43:25 +0200

    fatto un sacco di roba

--- app/src/main/java/com/example/iplay/MainActivity.kt
+++ app/src/main/java/com/example/iplay/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.iplay
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.goButton)
+        button.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
+
 }
