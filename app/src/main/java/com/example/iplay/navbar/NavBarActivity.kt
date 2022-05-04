c9098bff781d1812cebf98a9aab6f43857a11284
9Dalet <daniele.giarratano04@gmail.com>
2022-05-04 15:14:10 +0200

    implementata in kotlin navbar

--- app/src/main/java/com/example/iplay/navbar/NavBarActivity.kt
+++ app/src/main/java/com/example/iplay/navbar/NavBarActivity.kt
@@ -6,16 +6,24 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.iplay.R
 import com.example.iplay.chat.ChatActivity
 import com.example.iplay.create.CreateActivity
 import com.example.iplay.home.HomeActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class NavBarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nav_bar)
 
+        //Initialize the bottom navigation view
+        //create bottom navigation view object
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        val navController = findNavController(R.id.nav_fragment)
+        bottomNavigationView.setupWithNavController(navController)
 
     }
 
