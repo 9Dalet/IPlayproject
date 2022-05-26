7c1c1afeb607dfce49f73e937dd8b4b077ab04af
9Dalet <daniele.giarratano04@gmail.com>
2022-05-26 14:06:19 +0200

    dio fa i commit

--- app/src/main/java/com/example/iplay/registrati/RegistratiActivity.kt
+++ app/src/main/java/com/example/iplay/registrati/RegistratiActivity.kt
@@ -5,16 +5,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.core.content.ContextCompat.startActivity
 import com.example.iplay.EmailAccess.EmailLogin
 import com.example.iplay.R
 import com.example.iplay.home.HomeFragment
-import com.example.iplay.login.LoginActivity
-import com.example.iplay.navbar.NavBarActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
@@ -26,61 +22,47 @@ class RegistratiActivity : AppCompatActivity() {
     private lateinit var emptyEmail: EditText
     private lateinit var emptyPassword: EditText
     private lateinit var button: Button
-    private lateinit var email: String
-    private lateinit var password: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrati)
 
-        email = ""
-        password = ""
         auth = Firebase.auth
         emptyEmail = findViewById(R.id.editTextTextEmailAddress2)
         emptyPassword = findViewById(R.id.editTextTextPassword2)
         button = findViewById(R.id.button2)
 
+        val email = emptyEmail.text.toString()
+        val password = emptyPassword.text.toString()
+
         button.setOnClickListener {
-            if (emptyEmail.text.isNotEmpty() || emptyPassword.text.isNotEmpty()) {
-                email = emptyEmail.getText().toString()
-                password = emptyPassword.getText().toString()
-            } else {
-                Toast.makeText(this, "Enter valid data!", Toast.LENGTH_SHORT).show();
-                return@setOnClickListener
-            }
-            createAccount(email, password)
-            val intent = Intent(this, NavBarActivity::class.java)
+            val intent = Intent(this, ::class.java)
             startActivity(intent)
             finish()
         }
+
     }
 
-    //vedere se l'utente è già connesso
     public override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
-        if (currentUser != null) {
-            reload();
-        }
+        updateUI(currentUser)
     }
 
-
-    private fun createAccount(email: String, password: String) {
-        auth.createUserWithEmailAndPassword(email, password)
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "createUserWithEmail:success")
+                    Log.d(TAG, "signInWithEmail:success")
                     val user = auth.currentUser
                     updateUI(user)
                 } else {
                     // If sign in fails, display a message to the user.
-                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                    Toast.makeText(
-                        baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
                     updateUI(null)
                 }
             }
@@ -89,10 +71,4 @@ class RegistratiActivity : AppCompatActivity() {
     private fun updateUI(currentUser: FirebaseUser?) {
 
     }
-
-    private fun reload() {
-
-    }
 }
-
-
