d7c5595897675d1b40f202bf9e5d49193e805cad
Simone <simone.pedrini12@gmail.com>
2022-06-22 15:20:17 +0200

    diocane

--- app/src/main/java/com/example/iplay/home/HomeFragment.kt
+++ app/src/main/java/com/example/iplay/home/HomeFragment.kt
@@ -27,7 +27,6 @@ class HomeFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-
         arguments?.let {
         }
     }
