04664fa6255e135109b9d7a5cc07c22c67d8880e
Simone <simone.pedrini12@gmail.com>
2022-06-28 15:30:52 +0200

    finalmente si vedono ste cazzo di foto dioporco

--- app/src/main/java/com/example/iplay/search/SportEvent.kt
+++ app/src/main/java/com/example/iplay/search/SportEvent.kt
@@ -1,11 +1,21 @@
 package com.example.iplay.search
 
 import android.graphics.drawable.Drawable
+import android.view.ViewGroup
 import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.request.target.ViewTarget
 import com.google.firebase.storage.StorageReference
 import java.sql.Timestamp
 
-class SportEvent(var luogo: String, var numPersone: String, var data: String, var ora: String, var prezzo: String, var sport: String, var image: StorageReference) {
+class SportEvent(
+    var luogo: String,
+    var numPersone: String,
+    var data: String,
+    var ora: String,
+    var prezzo: String,
+    var sport: String,
+    var image: String
+) {
 }
 
